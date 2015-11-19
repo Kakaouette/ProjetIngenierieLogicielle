a64ec1d9da669ee9736d3f6c84f47a5656ab1289
miosoky <dorian.coqueron@hotmail.fr>
2015-11-19 11:20:53 +0100

    Création de la classe SupprimerUtilisateurAction, ajout de la methode delete dans CompteDAO et de la methode supprimerUtilisateur dans CompteService

---
+++ src/java/page/action/SupprimerUtilisateurAction.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package page.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modele.dao.CompteDAO;
+import modele.entite.Compte;
+import service.CompteService;
+
+/**
+ *
+ * @author dorian
+ */
+public class SupprimerUtilisateurAction implements Action {
+    
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response){
+        String session = request.getParameter("session");
+        if ("deco".equals(session)) {
+            request.getSession().invalidate();
+            return "index.jsp";
+        }       
+        String login = request.getParameter("login");
+
+        
+        CompteService compteService = new CompteService();
+        compteService.supprimerUtilisateur(login);
+        request.setAttribute("message", "Suppression effectuée");
+        return "listeUtilisateurs.jsp";
+    }
+}
