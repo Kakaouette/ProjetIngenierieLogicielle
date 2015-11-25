5e3502a41b99692e78e149b3a6b651891c0d1452
Kirako79 <etienne.meunier@yahoo.fr>
2015-11-25 22:09:18 +0100

    Création du fichier de tests pour DossierService.java.

---
+++ test/service/DossierServiceTest.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import modele.entite.Dossier;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Etienne
+ */
+public class DossierServiceTest {
+    DossierService instance = new DossierService();
+    
+    public DossierServiceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        instance = new DossierService();
+    }
+    
+    @After
+    public void tearDown() {
+        instance = null;
+    }
+
+    /**
+     * Test of getNewID method, of class DossierService.
+     */
+    @Test
+    public void testGetNewID() {
+        System.out.println("getNewID");
+        String expResult = "";
+        String result = instance.getNewID();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of ajouterDossier method, of class DossierService.
+     */
+    @Test
+    public void testAjouterDossier() throws Exception {
+        System.out.println("ajouterDossier");
+        Dossier dossier = null;
+        instance.ajouterDossier(dossier);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of regexIdDossierValide method, of class DossierService.
+     */
+    @Test
+    public void testRegexIdDossierValide() {
+        System.out.println("regexIdDossierValide");
+        String idDossier = "";
+        boolean expResult = false;
+        boolean result = instance.regexIdDossierValide(idDossier);
+        assertEquals(expResult, result);
+        // TODO
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getRegexIdDossier method, of class DossierService.
+     */
+    @Test
+    public void testGetRegexIdDossier() {
+        System.out.println("getRegexIdDossier");
+        String expResult = "";
+        String result = instance.getRegexIdDossier();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
