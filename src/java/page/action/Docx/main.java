2f4800d434a1b759202c49f7cbcf0b38cf5d6888
Jordan Drapeau <spike85200@hotmail.fr>
2015-12-03 13:20:10 +0100

    Ajout du programme test + lettres modeles

---
+++ src/java/page/action/Docx/main.java
@@ -0,0 +1,23 @@
+package page.action.Docx;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+
+/*
+* Différents tests
+*/
+public class main {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException, InvalidFormatException {
+		//String[] text = DocReader.readDocxFile("Test.docx");
+		//DocReader.createDoc("Hey");
+		//DocReader.readDocFile("C:\\Test.doc");
+		DocReader.replaceName("Test.docx","Dupont Jean");
+		DocReader.replaceFirstname("Test.docx","Dupont");
+		DocReader.replaceLastname("Test.docx","Jean");
+		ConvertDocxToPDF.convert("Test.docx");
+	}
+
+}
