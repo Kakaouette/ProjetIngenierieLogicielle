5495f23cf9e3c3f6ad9d86baecf8bc5f7115bb19
Kirako79 <etienne.meunier@yahoo.fr>
2015-12-07 12:54:51 +0100

    Correction des tests de la classe DossierService.

--- test/service/DossierServiceTest.java
+++ test/service/DossierServiceTest.java
@@ -8,15 +8,23 @@ package service;
 import service.exception.AjoutDossierInvalideException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import modele.dao.AdresseDAO;
+import modele.dao.CompteDAO;
 import modele.dao.DossierDAO;
 import modele.dao.EtudiantDAO;
+import modele.dao.FormationDAO;
+import modele.dao.HistoriqueDAO;
 import modele.entite.Adresse;
+import modele.entite.Compte;
 import modele.entite.Dossier;
 import modele.entite.Etudiant;
+import modele.entite.Formation;
+import modele.entite.Historique;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -75,8 +83,6 @@ public class DossierServiceTest {
         String result4 = instance.getNewID();
         dossierDAO.save(new Dossier(result4, new Date(), "testEtat", null, true, etudiant, null));
 
-        System.out.println("----result4:"+result4);
-        System.out.println("----result4_:"+dernierID.substring(0,11)+(Integer.parseInt(dernierID.substring(11))+3));
         ////        / DATE DU DOSSIER        / /    ID DOSSIER                      ////
         assertEquals(dernierID.substring(0,11)+(Integer.parseInt(dernierID.substring(11))+0), result1);
         assertEquals(dernierID.substring(0,11)+(Integer.parseInt(dernierID.substring(11))+1), result2);
@@ -90,7 +96,16 @@ public class DossierServiceTest {
     @Test
     public void testAjouterDossier() throws Exception {
         System.out.println("ajouterDossier");
-        Dossier dossier = new Dossier(new Date(), "UnEtat", "UneLettre", false, null, null);
+        Etudiant etudiant = new EtudiantDAO().getEtudiantByNomPrenom("Jean","Pierre");
+        Formation formation = new FormationDAO().getFormationByIntitule("M1 ICONE");
+        Compte c = new CompteDAO().getById(1);
+        Historique historique = new Historique(new Date(), "Message", "Action", c);
+        List<Historique> sesHistoriques = new ArrayList<>();
+        sesHistoriques.add(historique);
+        ///   (Date date, String etat, String lettre, boolean admissible, Etudiant etudiant, Formation demandeFormation, List<Historique> historique)
+        Dossier dossier = new Dossier(new Date(), "UnEtat", "UneLettre", false, etudiant, formation, sesHistoriques);
+        
+        /// //////////////////////////// TEST AVEC UN ID INCORRECTE ////////////////////////////
         dossier.setId("NeDoitPasPasser");
         try {
             instance.ajouterDossier(dossier);
@@ -99,19 +114,23 @@ public class DossierServiceTest {
         catch(AjoutDossierInvalideException e)
         {
         }
-        dossier.setId("pst151119700");
+        /// //////////////////////////// TEST AVEC UN ID CORRECTE ////////////////////////////
         Adresse uneAdresse = new Adresse("test_codePoste", "test_Ville");
         Etudiant unEtudiant = new Etudiant("test_Nom", "test_Prenom", "test_adressePostale", "test_Homme", uneAdresse);
+        dossier = new Dossier(new Date(), "UnEtat", "UneLettre", false, unEtudiant, formation, sesHistoriques);
+        dossier.setId("pst151119750");
         instance.ajouterDossier(dossier);
         
         /// on vérifi son existance.
-        Dossier cpt = new DossierDAO().getById("pst151119700");
+        Dossier cpt = new DossierDAO().getById("pst151119750");
         assertEquals(dossier, cpt);
-        
-        new DossierDAO().delete(dossier);
         
         new AdresseDAO().delete(uneAdresse);
         new EtudiantDAO().delete(unEtudiant);
+        new DossierDAO().delete(dossier);
+        
+        //new AdresseDAO().delete(uneAdresse);
+        //new EtudiantDAO().delete(unEtudiant);
         //dossierDAO.
     }
 
@@ -123,15 +142,15 @@ public class DossierServiceTest {
         System.out.println("regexIdDossierValide");
         try {
             assertEquals(true, instance.regexIdDossierValide("pst151120110"));
-            assertEquals(true, instance.regexIdDossierValide("pst251220151"));
+            assertEquals(false, instance.regexIdDossierValide("pst251220151"));
             assertEquals(true, instance.regexIdDossierValide("pst171119922"));
             assertEquals(true, instance.regexIdDossierValide("pst151120113"));
+            assertEquals(true, instance.regexIdDossierValide("pst150020111"));
             assertEquals(false, instance.regexIdDossierValide("pst"));
             assertEquals(false, instance.regexIdDossierValide(""));
             assertEquals(false, instance.regexIdDossierValide("pst451120110"));
             assertEquals(false, instance.regexIdDossierValide("pst601120111"));
             assertEquals(false, instance.regexIdDossierValide("pst151920110"));
-            assertEquals(false, instance.regexIdDossierValide("pst150020111"));
         } catch (Exception ex) {
             Logger.getLogger(DossierServiceTest.class.getName()).log(Level.SEVERE, null, ex);
         }
