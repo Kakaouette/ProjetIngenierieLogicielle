4e654115f8dbe566776425d91c45086026d92859
Drachenfels <Drachenfels@Drachenfels-PC>
2015-12-14 12:51:42 +0100

    Modification des pages d'accueil pour chaque rôle.

--- src/java/page/action/accueil/VoirIndexAction.java
+++ src/java/page/action/accueil/VoirIndexAction.java
@@ -11,6 +11,7 @@ import modele.entite.Compte;
 import modele.entite.TypeCompte;
 import page.action.Action;
 import page.action.dossier.AfficherInformationsDossiersAction;
+import page.action.dossier.ConsulterDossierAction;
 
 /**
  *
@@ -28,18 +29,46 @@ public class VoirIndexAction implements Action {
         else{
             if(compte.getType()== TypeCompte.secrétaire_formation)
             {
-                // traitement a effectuer pour la page d'accueil de la secretaire generale
+                // traitement a effectuer pour la page d'accueil de la secretaire de formation
                 return new AfficherInformationsDossiersAction().execute(request, response); // charge les dossiers + retourne "listeDossiers.jsp"
             }
             else if(compte.getType()== TypeCompte.secrétaire_inscription)
             {
-                // traitement a effectuer pour la page d'accueil de la secretaire formation
+                // traitement a effectuer pour la page d'accueil de la secretaire d'inscriptions
                 return new AfficherInformationsDossiersAction().execute(request, response); // charge les dossiers + retourne "listeDossiers.jsp"
+            }else if(compte.getType()== TypeCompte.responsable_commission)
+            {
+                // traitement a effectuer pour la page d'accueil du responsable de commission
+                return new AfficherInformationsDossiersAction().execute(request, response); // charge les dossiers + retourne "listeDossiers.jsp"
+            }else if(compte.getType()== TypeCompte.responsable_formation)
+            {
+                // traitement a effectuer pour la page d'accueil du responsable de formation
+                return "accueil.jsp";
+                //return new AfficherInformationsDossiersAction().execute(request, response); // charge les dossiers + retourne "listeDossiers.jsp"
+            }else if(compte.getType()== TypeCompte.responsable_administrative)
+            {
+                // traitement a effectuer pour la page d'accueil du responsable administratif
+                return "accueil.jsp";
+                //return new AfficherInformationsDossiersAction().execute(request, response); // charge les dossiers + retourne "listeDossiers.jsp"
+            }else if(compte.getType()== TypeCompte.directeur_pole)
+            {
+                // traitement a effectuer pour la page d'accueil du directeur du pôle
+                return "accueil.jsp";
+                //return new AfficherInformationsDossiersAction().execute(request, response); // charge les dossiers + retourne "listeDossiers.jsp"
+            }else if(compte.getType()== TypeCompte.admin)
+            {
+                // traitement a effectuer pour la page d'accueil de l'administrateur
+                return "accueil.jsp";
+                //return new AfficherInformationsDossiersAction().execute(request, response); // charge les dossiers + retourne "listeDossiers.jsp"
             }
             else
             {
                 return "accueil.jsp";
             }
+            /*switch(compte.getType()){
+                case TypeCompte.responsable_commission:{
+                    
+                }*/
         }
     }
 }
